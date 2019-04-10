38fac725a35a971132f0c65f46dabc18ae31242e
alexwu <alexwu@kscthinktank.com.tw>
2019-04-10 21:45:49 +0800

    reply location message test

--- bot.js
+++ bot.js
@@ -7,19 +7,41 @@ const bot = linebot({
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
+const shareLocation = require('./reply/shareLocation.json')
 
 bot.on('message', event => {
     if (event.message.type === "text") {
-        event.reply(event.message.text)
-            .then(data => {
-                console.log(data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
+        console.log(event.message.text)
+        if (event.message.text === '查詢附近停車場') {
+            event
+                .reply(shareLocation)
+                .then(data => console.log(data))
+                .catch(err => console.log(err))
+
+        } else {
+            event.reply(event.message.text)
+                .then(data => {
+                    console.log(data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
 
     } else if (event.message.type === "location") {
-        console.log(event)
+        event.reply({
+            type: 'location',
+            title: 'this place',
+            address: event.message.address,
+            latitude: event.message.latitude,
+            longitude: event.message.longitude
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
     
